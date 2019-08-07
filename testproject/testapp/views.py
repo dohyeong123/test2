cbb555b149d02d197af5566be6519bef43d695c1
sudond <sudond@naver.com>
2019-08-07 22:46:51 +0900

    social login

--- testproject/testapp/views.py
+++ testproject/testapp/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'testapp/home.html')
-
+def login(request):
+    return render(request,'testapp/login.html')
 def logout(request):
     return render(request,'testapp/logout.html')
 def signup(request):
--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -5,8 +5,9 @@ from django.contrib.auth import views as auth_view
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',testapp.views.home, name='home'),
-    path('login/',auth_view.LoginView.as_view(), name='login'),
+    path('login/',testapp.views.login, name='login'),
     path('logout/',testapp.views.logout, name='logout'),
     path('signup/',testapp.views.signup, name='signup'),
+    path('accounts/',include('allauth.urls')),
     
 ]
